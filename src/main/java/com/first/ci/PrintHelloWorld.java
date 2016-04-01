f38d945353a2317a3460a1e7a70a2eddbb802fd7
Abishua Prashanth <vadulasp@gmail.com>
2016-04-01 18:59:13 -0400

    First Commit

---
+++ src/main/java/com/first/ci/PrintHelloWorld.java
@@ -0,0 +1,12 @@
+package com.first.ci;
+
+/**
+ * Created by vadulasp on 3/30/16.
+ */
+public class PrintHelloWorld {
+
+    public String print(){
+        return "HelloWorld";
+
+    }
+}
---
+++ src/test/java/com/first/ci/PrintHelloWorldTest.java
@@ -0,0 +1,20 @@
+package com.first.ci;
+
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+
+/**
+ * Created by vadulasp on 3/30/16.
+ */
+public class PrintHelloWorldTest {
+
+
+    @Test
+    public void testPrintHelloWorld(){
+        PrintHelloWorld p  = new PrintHelloWorld();
+        String  s = p.print();
+        assertNotNull(s);
+    }
+}
