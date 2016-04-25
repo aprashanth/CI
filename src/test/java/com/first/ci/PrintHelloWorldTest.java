7b8bf8c25ae4f5fece46f9f3f21d5194fffac2e6
aprashanth <vadulasp@gmail.com>
2016-04-25 11:34:35 -0400

    New Commit3

--- src/test/java/com/first/ci/PrintHelloWorldTest.java
+++ src/test/java/com/first/ci/PrintHelloWorldTest.java
@@ -17,6 +17,6 @@ public class PrintHelloWorldTest {
         PrintHelloWorld p  = new PrintHelloWorld();
         String  s = p.print();
         assertNotNull(s);
-        assertEquals("HelloWorl", s);
+        assertEquals("HelloWorld", s);
     }
 }
